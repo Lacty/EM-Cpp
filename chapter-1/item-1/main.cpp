6ef03f3bb7c7da8f14097e60ecaeeaea09f08f70
Lacty <akira206@gmail.com>
2016-07-21 18:25:49 +0900

    update

--- chapter-1/item-1/main.cpp
+++ chapter-1/item-1/main.cpp
@@ -24,24 +24,22 @@ template<typename T>
 void func1(T& src) {}
 
 int main() {
-  {
-    // ParamTypeが参照でもポインタでもない場合は値渡し(コピー)となる
-    int x = 0;
-    const int cx = x;
-    const int& rx = x;
-    func0(x);  // ParamType is int
-    func0(cx); // ParamType is int
-    func0(rx); // ParamType is int
-  }
+  int x = 0;
+  const int cx = x;
+  const int& rx = x;
 
-  {
-    // ParamTypeが参照またはポインタで、ユニヴァーサル参照出ない場合
-    // 引数の参照性は無視される
-    int x = 0;
-    const int cx = x;
-    const int& rx = x;
-    func1(x); // T is int : ParamType is int&
-    func1(cx); // T is const int : ParamType is const int&
-    func1(rx); // T is const int : ParamType is const int&
-  }
+
+  // ParamTypeが参照でもポインタでもない場合は値渡し(コピー)となる
+  
+  func0(x);  // ParamType is int
+  func0(cx); // ParamType is int
+  func0(rx); // ParamType is int
+
+
+  // ParamTypeが参照またはポインタで、ユニヴァーサル参照出ない場合
+  // 引数の参照性は無視される
+  
+  func1(x);  // T is int : ParamType is int&
+  func1(cx); // T is const int : ParamType is const int&
+  func1(rx); // T is const int : ParamType is const int&
 }
