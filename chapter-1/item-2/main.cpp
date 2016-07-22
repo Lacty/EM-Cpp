96eadc3bb5d4d61b9f697ec01ff8f8f58877e4ad
Lacty <akira206@gmail.com>
2016-07-22 16:51:30 +0900

    update item-2
    配列または関数名はポインタに成り下がる

--- chapter-1/item-2/main.cpp
+++ chapter-1/item-2/main.cpp
@@ -6,6 +6,8 @@
   auto もtemaplteの型推論と同一である
 */
 
+void func(int i, float f) {}
+
 int main() {
   // auto は Tを担い
   // 型指定子がParamTypeを担う
@@ -17,4 +19,15 @@ int main() {
   auto&& uref2 = cx; // uref2 is const int&
   auto&& uref3 = rx; // uref3 is const int&
   auto&& uref4 = 0;  // uref4 is int&&
+
+  
+  // 配列および関数名は非参照型指定子のポインタに成り下がる
+  
+  const char name[] = "I'm Yanai"; // name is const char[10]
+  
+  auto arr1 = name; // arr1 is const char*
+  auto& arr2 = name; // arr2 is const char(&)[10]
+  
+  auto func1 = func; // func1 is void (*)(int, float)
+  auto func2 = func; // func2 is void (&)(int, float)
 }
