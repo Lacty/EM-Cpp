35ad6dff352ef987581ce81a4e438e657fbc64c8
Lacty <akira206@gmail.com>
2016-07-22 16:36:44 +0900

    update item-2
    auto&& について記述

--- chapter-1/item-2/main.cpp
+++ chapter-1/item-2/main.cpp
@@ -1,6 +1,6 @@
 
 #include <iostream>
-
+#include <typeinfo>
 
 /*
   auto もtemaplteの型推論と同一である
@@ -12,4 +12,9 @@ int main() {
   auto x = 0;         // auto is int : 型指定子 is auto
   const auto cx = x;  // auto is int : 型指定子 is const auto
   const auto& rx = x; // auto is int : 型指定子 is const auto&
+
+  auto&& uref1 = x;  // uref1 is int&
+  auto&& uref2 = cx; // uref2 is const int&
+  auto&& uref3 = rx; // uref3 is const int&
+  auto&& uref4 = 0;  // uref4 is int&&
 }
