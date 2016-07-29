f139b89af411e70db5fc330e5f26a2e52146ba8b
Lacty <akira206@gmail.com>
2016-07-29 13:36:56 +0900

    update item-7

--- chapter-3/item-7.cpp
+++ chapter-3/item-7.cpp
@@ -14,4 +14,23 @@ int main() {
 
   int sum(x + y + z);   // ok
   int sum = x + y + z;  // ok
+
+  /*
+    波括弧は、非スタティックなメンバ変数のデフォルト値の指定にも使える
+
+    class Hoge {
+      int x{ 0 }; // ok
+      int y = 0;  // ok
+      int z(0);   // err
+    };
+  */
+
+  /*
+    コピー不可なオブジェクト(std::atomic)は波括弧、丸括弧で初期化可能だが
+    [=]での初期化はできない
+  */
+
+  std::atomic<int> aa{ 0 }; // ok
+  std::atomic<int> ab(0);   // ok
+  std::atomic<int> ac = 0;  // err
 }
